7a3669ef2d9ebee149d260bb2426fa07da565d00
The5061696e <96197730+The5061696e@users.noreply.github.com>
2021-12-23 18:14:47 +0100

    Create hello world.cs

---
+++ hello world.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ppk_main
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Helloss World!");
+        }
+    }
+}
